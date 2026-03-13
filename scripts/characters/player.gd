6420a5aa0cdc1ab72c7ee322067223c907f55f54
Bryan Davis <fire00f1y@gmail.com>
2026-03-13 12:27:56 -0400

    movable character and collision

---
+++ scripts/characters/player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+const SPEED: float = 300.0
+const JUMP_VELOCITY: float = -400.0
+
+func _physics_process(delta: float) -> void:
+	var input_vector: Vector2 = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	input_vector = input_vector.normalized()
+
+	velocity = input_vector * SPEED
+	move_and_slide()
