ef6796863d4df4fe7d30a5a86fbf1bd0b62ec9ae
Bryan Davis <fire00f1y@gmail.com>
2026-03-13 14:39:51 -0400

    autotiling works! new camera

--- scripts/characters/player.gd
+++ scripts/characters/player.gd
@@ -4,10 +4,13 @@ const SPEED: float = 300.0
 const JUMP_VELOCITY: float = -400.0
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("menu"):
+		get_tree().quit(0)
+	
 	var input_vector: Vector2 = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	input_vector = input_vector.normalized()
-
+	
 	velocity = input_vector * SPEED
 	move_and_slide()
