dede1294c8a82e753ce5f857a26852435a3d43c9
Bryan Davis <fire00f1y@gmail.com>
2026-03-18 10:57:27 -0400

    on screen controls

---
+++ scripts/ui/element_button.gd
@@ -0,0 +1,22 @@
+@tool
+extends Control
+
+@export var button_text: String:
+	set(value):
+		button_text = value
+		if is_node_ready() and button_label:
+			button_label.text = value
+
+@export var button_icon: Texture2D:
+	set(value):
+		button_icon = value
+		if is_node_ready() and button_image:
+			button_image.texture = value
+
+@onready var button_label: Label = $VBoxContainer/ButtonLabel
+@onready var button_image: TextureRect = $VBoxContainer/ButtonImage
+
+func _ready() -> void:
+	button_label.text = button_text
+	if button_icon:
+		button_image.texture = button_icon
