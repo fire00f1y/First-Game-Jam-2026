dfbc8b14c917c52ae1fd83d35912ca36ffa62406
Bryan Davis <fire00f1y@gmail.com>
2026-03-18 02:14:04 -0400

    clean up file tree

---
+++ clean.ps1
@@ -0,0 +1,13 @@
+﻿$files = git show --stat --name-only --format="" c5f0158716efd636214a2f56b5eee9689f9b3acc
+$files = $files | Where-Object { $_ -ne "" }
+$files
+
+# write to paths file
+$files | Out-File -Encoding utf8 paths.txt
+
+# run filter-repo
+git filter-repo --paths-from-file paths.txt --invert-paths
+
+# re-add remote and force push
+git add .
+git commit -m "clean up file tree"
