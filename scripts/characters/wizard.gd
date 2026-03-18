93b20c8ccd038bd786b0cac5fdcd0d2203d93a08
Bryan Davis <fire00f1y@gmail.com>
2026-03-18 02:01:26 -0400

    add spell rune system

--- scripts/characters/wizard.gd
+++ scripts/characters/wizard.gd
@@ -18,6 +18,40 @@ var combo_timing_window_open: bool = false
 
 var enemies_hit_in_attack: Array[Node] = []
 
+const spell_rune_path_prefix : String = "res://assets/tiles/48 Magic Runes Pixel Art Icon Pack/PNG/Transperent/runes/"
+const fire_icon : String = spell_rune_path_prefix + "Icon1.png"
+const electric_icon : String = spell_rune_path_prefix + "Icon20.png"
+const ice_icon : String = spell_rune_path_prefix + "Icon10.png"
+const earth_icon : String = spell_rune_path_prefix + "Icon45.png"
+
+@export var element_textures: Dictionary = {
+	"fire": preload(fire_icon),
+	"electric": preload(electric_icon),
+	"ice": preload(ice_icon),
+	"earth": preload(earth_icon)
+}
+
+var incantation: Array = []
+@onready var slots : Array[Sprite2D] = [$IncantationRunes/rune1, $IncantationRunes/rune2, $IncantationRunes/rune3, $IncantationRunes/rune4]
+
+func add_element(element: String):
+	if incantation.size() >= 4:
+		incantation.pop_front()
+	incantation.append(element)
+	_refresh_display()
+	
+func _refresh_display():
+	for i : int in 4:
+		if i < incantation.size():
+			slots[i].texture = element_textures[incantation[i]]
+			slots[i].visible = true
+		else:
+			slots[i].visible = false
+
+func clear_incantation():
+	incantation.clear()
+	_refresh_display()
+
 func _ready() -> void:
 	hitbox.body_entered.connect(_on_hitbox_entered)
 
@@ -45,6 +79,18 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_just_pressed("block") and not is_attacking:
 		_handle_attack("attack1")
 	
+	if Input.is_action_just_pressed("fire"):
+		add_element("fire")
+	
+	if Input.is_action_just_pressed("ice"):
+		add_element("ice")
+
+	if Input.is_action_just_pressed("electric"):
+		add_element("electric")
+	
+	if Input.is_action_just_pressed("earth"):
+		add_element("earth")
+	
 	if not is_attacking:
 		_handle_movement(delta)
 
@@ -52,6 +98,7 @@ func _physics_process(delta: float) -> void:
 # this handles animation transition
 func _handle_attack(attack: String):
 	enemies_hit_in_attack.clear()
+	clear_incantation()
 	is_attacking = true
 	anim.play(attack)
 	await anim.animation_finished
